6f1ed210141206b893ada5683396828f04ae3bd5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-13 21:13:29 +0530

    turned on cors for socket.io

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+import { SocketProvider } from "./context/SocketProvider.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
