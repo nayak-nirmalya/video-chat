a2490660c1a7f09296c17a7adaad2297d1019834
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-14 16:53:43 +0530

    new screen RoomScreen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
+
 import { LobbyScreen } from "./screens/Lobby";
+import { RoomScreen } from "./screens/Room";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomScreen />} />
       </Routes>
     </>
   );
