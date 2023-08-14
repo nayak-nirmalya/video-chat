278d7e14f0051f769e4183358d6595f131986187
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-14 17:18:03 +0530

    show call btn if another user is present in same room

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useState } from "react";
 
 import { useSocket } from "../hooks/useSocket";
 
 export function RoomScreen() {
   const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
 
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log({ email, id });
+    setRemoteSocketId(id);
   }, []);
 
   useEffect(() => {
@@ -17,5 +19,12 @@ export function RoomScreen() {
     };
   }, [socket, handleUserJoined]);
 
-  return <div>RoomScreen</div>;
+  return (
+    <div>
+      <h1>RoomScreen</h1>
+      <h4>{remoteSocketId ? "Connected" : "No One in this Room."}</h4>
+
+      {remoteSocketId && <button>CALL</button>}
+    </div>
+  );
 }
